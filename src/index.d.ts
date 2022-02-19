d4b2101ba5b120da7b02cb8e8ac993d693adce57
Aprillion <peter.hozak@gmail.com>
2022-02-19 21:45:36 +0100

    fix types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,11 +1,9 @@
 declare function AutoHeight(
   props: {
-    children: JSX.Element | ((updateHeight: () => void) => JSX.Element)
-    element: keyof React.ReactHTML | React.ComponentType
-    className: string
-  } & {
-    [prop: string]: React.ComponentProps<'div'>
-  },
+    children?: JSX.Element | ((updateHeight: () => void) => JSX.Element) | false
+    element?: keyof React.ReactHTML | React.ComponentType
+    className?: string
+  } & Record<string, unknown>,
 ): JSX.Element
 
 export default AutoHeight
